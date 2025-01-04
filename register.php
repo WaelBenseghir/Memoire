59579ec6d3f23db98c19c8285ca76d9e70842daf
WaelBenseghir <132686385+WaelBenseghir@users.noreply.github.com>
2025-01-04 16:24:12 +0100

    p7

--- register.php
+++ register.php
@@ -7,6 +7,7 @@ if(isset($_POST['signUp'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     $checkEmail = "SELECT * FROM user WHERE email = '$email'";
     $result = $conn->query($checkEmail);
@@ -15,9 +16,9 @@ if(isset($_POST['signUp'])){
     }
     else{
         $insertQuery = "INSERT INTO user(name, email, password)
-                        VALUES ('$name', '$email', '$password')";
+                        VALUES ('$name', '$email', '$hashed_password')";
             if($conn->query($insertQuery)==TRUE){
-                header("location:index.html");
+                header("location:home.php");
             }
             else{
                 echo "Error:".$conn->error;
@@ -29,17 +30,23 @@ if(isset($_POST['signIn'])){
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM user WHERE email='$email' and password='$password'";
+    $sql = "SELECT * FROM user WHERE email='$email'";
     $result = $conn->query($sql);
+    
     if($result->num_rows>0){
-        session_start();
         $row = $result->fetch_assoc();
-        $_SESSION['email'] = $row['email'];
-        header("location:index.html");
-        exit();
-    }
-    else{
+
+        if(password_verify($password, $row['password'])){
+            session_start();
+            $row = $result->fetch_assoc();
+            $_SESSION['email'] = $row['email'];
+            header("location:home.php");
+            exit();
+        } 
+        else{
         echo "Incorrect Email or Password";
     }
+    }
+    
 }
 ?>
