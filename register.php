3ae5408a25cd45bac569947eb8976e248b7fc7ae
WaelBenseghir <132686385+WaelBenseghir@users.noreply.github.com>
2025-02-12 14:20:08 +0100

    t2

---
+++ register.php
@@ -0,0 +1,50 @@
+<?php
+
+include 'connect.php';
+
+
+if(isset($_POST['signUp'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $checkEmail = "SELECT * FROM user WHERE email = '$email'";
+    $result = $conn->query($checkEmail);
+    if($result->num_rows>0){
+        echo "Email Address Already Exists! ";
+    }
+    else{
+        $insertQuery = "INSERT INTO user(name, email, password)
+                        VALUES ('$name', '$email', '$hashed_password')";
+            if($conn->query($insertQuery)==TRUE){
+                header("location:main.html");
+            }
+            else{
+                echo "Error:".$conn->error;
+            }
+    }
+}
+
+if(isset($_POST['signIn'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM user WHERE email='$email'";
+    $result = $conn->query($sql);
+    if($result->num_rows>0){
+        $row = $result->fetch_assoc();
+        if(password_verify($password, $row['password'])){
+            session_start();
+            $row = $result->fetch_assoc();
+            $_SESSION['email'] = $row['email'];
+            header("location:main.html");
+            exit();
+        } 
+    else{
+        echo "Incorrect Email or Password";
+    }
+    }
+    
+}
+?>
